48ed561aa48a9971e7bed3cab672cc5aa3fcf60a
vietvu <viet.vu.95@hotmail.com>
2022-12-17 12:08:24 +1100

    testing api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
+require "json"
+require "open-uri"
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # url = "http://api.marketstack.com/v1/eod?access_key=0f7424aef84ceea4f0ac1b4db80ec5c5&symbols=AAPL"
+    # user_serialized = URI.open(url).read
+    # @user = JSON.parse(user_serialized)
   end
 end
