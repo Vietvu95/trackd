9d6cd62d195f5db1b8812222a43c4bd82c8b36aa
vietvu <viet.vu.95@hotmail.com>
2023-01-05 19:19:59 +1100

    test

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,6 +1,10 @@
 require "json"
 require "open-uri"
 class AssetsController < ApplicationController
+  url = “https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{ENV['API_KEY']}”
+    #url = “https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=9da0f106-d8b3-4138-865c-09975e3c1f58”
+    uri = URI.open(url).read
+    @tickers = JSON.parse(uri)
 
   def edit
     @asset = Asset.find(params[:id])
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,18 +7,15 @@ class PortfoliosController < ApplicationController
 
   def new
     if params[:query].present?
-      tickers = params[:query]
       begin
-        url = "http://api.marketstack.com/v1/eod?access_key=0f7424aef84ceea4f0ac1b4db80ec5c5&symbols=#{tickers}"
-        assets_serialized = URI.open(url).read
-        @assets = JSON.parse(assets_serialized)
+       @assets = MarketStackApiManager.new(params[:query]).get_data
       rescue => e
         @error = "not valid ticker"
       end
     end
     @portfolio = Portfolio.new
-    @portfolio.name = @assets["data"].first["symbol"]
-    @portfolio.user = current_user
+    # @portfolio.name = @assets["data"].first["symbol"]
+    # @portfolio.user = current_user
   end
 
   def create
