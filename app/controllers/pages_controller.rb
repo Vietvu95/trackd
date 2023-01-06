2fead44cf66c893b04ae31af2d01301e01a0d889
chhomtythya norong <nctythya@yahoo.com>
2023-01-06 21:37:07 +1100

    first step stock API

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+require "json"
+require "open-uri"
+
+
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :home ]
+
+  def home
+    if params[:query].present?
+      begin
+        @assets = MarketStackApiManager.new(params[:query]).get_data
+      rescue => e
+        @error = "not valid ticker"
+      end
+    end
+
+    myurl = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=L73EE7ZUNUZ4DKW0'
+    test = URI.open(myurl).read
+      @hello = JSON.parse(test)
+
+
+  end
+end
---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,50 @@
+class PortfoliosController < ApplicationController
+  # before_action :set_portfolio, only: [:new, :create, :show, :edit, :update, :destroy]
+
+  def index
+    @portfolios = Portfolio.all
+  end
+
+  def new
+    if params[:query].present?
+      begin
+       @assets = MarketStackApiManager.new(params[:query]).get_data
+      rescue => e
+        @error = "not valid ticker"
+        # begin
+        #   @assets = coin.new(params[:query]).get_data
+        # rescue => e
+        #   @error = "not valid ticker"
+      #  end
+      end
+    end
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new
+    @portfolio.user = current_user
+    @portfolio.name = @assets
+    if @portfolio.save
+      redirect_to portfolio_path(@portfolio)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @portfolio = Portfolio.find(params[:id])
+    @portfolio_assets = @portfolio.portfolio_assets
+  end
+
+
+
+  private
+  def portfolio_params
+    params.require(:portfolio).permit(:name, :portfolio_value)
+  end
+
+  def set_portfolio
+    @portfolio = Portfolio.find(params[:id])
+  end
+end
