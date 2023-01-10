cb52f677d7fc469e5de2ac72f5fba65a7a9918d4
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2023-01-10 21:37:44 +1030

    created 3 new sections + get started at hp, fixed the top 5 forms.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,5 +11,11 @@ class PagesController < ApplicationController
         @error = "not valid ticker"
       end
     end
+    begin
+      res = URI.open("https://api.marketaux.com/v1/news/all?api_token=#{ENV['MARKETAUX_API_KEY']}&language=en").read
+      @marketaux_hash = JSON.parse(res)
+    rescue => exception
+      @marketaux_hash = {"data" => []}
+    end
   end
 end
