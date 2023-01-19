bc48e76ad1e072736cd11a86c9e70af995f58912
vietvu <viet.vu.95@hotmail.com>
2023-01-19 20:07:19 +1100

    fixed P/L issues

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,3 @@ class DashboardController < ApplicationController
     @total_pl = (@total_current_value - @total_purchase_value).round(2)
   end
 end
-
-#find total purchase price of all portfolios
-#to calculate P/L subtract total_purchase_value  from @total_current_value
-#display in view
